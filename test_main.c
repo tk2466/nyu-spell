161656fa347866dfc8b86654bbf9ec170b98b523
tk2466 <55135576+tk2466@users.noreply.github.com>
2019-09-27 04:01:17 -0400

    Added new test cases

--- test_main.c
+++ test_main.c
@@ -15,6 +15,62 @@ START_TEST(test_check_word_normal)
 }
 END_TEST
 
+START_TEST(test_check_word_punctuantion)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    const char* correct_word = "Justice";
+    const char* incorrect_word = "Caoimhín!";
+    ck_assert(check_word(correct_word, hashtable));
+    ck_assert(!check_word(incorrect_word, hashtable));
+}
+END_TEST
+
+START_TEST(test_check_word_space)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    const char* correct_word = "Justice";
+    const char* incorrect_word = "Jus tice";
+    ck_assert(check_word(correct_word, hashtable));
+    ck_assert(!check_word(incorrect_word, hashtable));
+}
+END_TEST
+
+START_TEST(test_check_word_speacial)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    const char* correct_word = "Justice";
+    const char* incorrect_word = "@#Justice";
+    ck_assert(check_word(correct_word, hashtable));
+    ck_assert(!check_word(incorrect_word, hashtable));
+}
+END_TEST
+
+START_TEST(test_check_words_normal)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    char* expected[3];
+    expected[0] = "sogn";
+    expected[1] = "skyn";
+    expected[2] = "betta";
+    char *misspelled[MAX_MISSPELLED];
+    FILE *fp = fopen("test1.txt", "r");
+    int num_misspelled = check_words(fp, hashtable, misspelled);
+    ck_assert(num_misspelled == 3);
+    bool test = strlen(misspelled[0]) == strlen(expected[0]);
+    int len1 = strlen(misspelled[0]);
+    int len2 = strlen(expected[0]);
+    ck_assert_msg(test, "%d!=%d", len1, len2);
+    ck_assert_msg(strcmp(misspelled[0], expected[0]) == 0);
+    ck_assert_msg(strcmp(misspelled[1], expected[1]) == 0);
+    ck_assert_msg(strcmp(misspelled[2], expected[2]) == 0);
+}
+END_TEST
+
+
 START_TEST(test_check_word_buffer_overflow)
 {
     hashmap_t hashtable[HASH_SIZE];
@@ -35,6 +91,10 @@ check_word_suite(void)
     suite = suite_create("check_word");
     check_word_case = tcase_create("Core");
     tcase_add_test(check_word_case, test_check_word_normal);
+    tcase_add_test(check_word_case, test_check_word_punctuantion);
+    tcase_add_test(check_word_case, test_check_word_space);
+    tcase_add_test(check_word_case, test_check_word_speacial);
+    tcase_add_test(check_word_case, test_check_words_normal);
     tcase_add_test(check_word_case, test_check_word_buffer_overflow);
     suite_add_tcase(suite, check_word_case);
 
@@ -47,7 +107,7 @@ main(void)
     int failed;
     Suite *suite;
     SRunner *runner;
-    
+
     suite = check_word_suite();
     runner = srunner_create(suite);
     srunner_run_all(runner, CK_NORMAL);
